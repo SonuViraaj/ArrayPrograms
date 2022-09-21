fa19c4d1e1f44fceddfb5f01b48cdd5ac3a4cd91
Sonu Kumar <110598115+SonuViraaj@users.noreply.github.com>
2022-09-21 23:04:57 +0530

    Add files via upload

---
+++ invertedhalf_rotatedby180.java
@@ -0,0 +1,22 @@
+package patterns;
+
+public class invertedhalf_rotatedby180 {
+    
+    public static void main(String args[]){ 
+        int n=4;
+
+        //outer loop
+        for(int i=1;i<=n;i++){
+            //inner loop-> space print
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //inner loop-> star print
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ solidrectangle.java
@@ -0,0 +1,17 @@
+package patterns;
+import java.util.*;
+
+public class solidrectangle {
+    public static void main(String args[]){
+        int n=4;
+        int m=5;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+    }
+    
+}
